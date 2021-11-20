b699d12154a9c316843f428b7af46dd4975921cc
MartaAndriychak <bolubash@meta.ua>
2021-11-20 09:06:34 +0200

    initial commit
    Add script for creating tables and script for populating

---
+++ create_database.sql
@@ -0,0 +1,150 @@
+
+CREATE DATABASE Travel_agency;
+
+USE Travel_agency;
+
+CREATE TABLE Passport_type (
+	ID INT NOT NULL identity,
+	Discription varchar(255) NOT NULL,
+	Constraint PK_Passport_type  PRIMARY KEY (ID));
+
+CREATE TABLE Person (
+    ID INT NOT NULL identity,
+	Name varchar(255) NOT NULL,
+	Last_name varchar(255) NOT NULL,
+	Middle_name varchar(255) NOT NULL,
+	Date_of_birth DATETIME NOT NULL,
+	Address varchar(255) NOT NULL,
+	Mobile_number varchar(255) NOT NULL,
+	Constraint PK_Person PRIMARY KEY (ID));
+
+CREATE TABLE Passport_data (
+	ID INT NOT NULL identity ,
+	ID_passport_type INT NOT NULL,
+	ID_person INT NOT NULL,
+	Details varchar(255) NOT NULL,
+	Constraint FK_Passport_type_passport_data  FOREIGN KEY (ID_passport_type) references Passport_type (ID),
+	Constraint FK_Person_passport_data  FOREIGN KEY (ID_person) references Person (ID),
+	Constraint PK_Passport_data   PRIMARY KEY (ID));
+
+
+CREATE TABLE Manager (
+  ID INT NOT NULL identity,
+  Name varchar(255) NOT NULL,
+  Last_name varchar(255) NOT NULL,
+  Middle_name varchar(255) NOT NULL,
+  Mobile_number varchar(255) NOT NULL,
+  Constraint PK_Manager PRIMARY KEY (ID));
+
+ CREATE TABLE Country (
+	ID INT NOT NULL identity,
+	Country varchar(255) NOT NULL,
+	Constraint PK_Country  PRIMARY KEY (ID));
+
+
+CREATE TABLE City (
+	ID INT NOT NULL identity,
+	City varchar(255) NOT NULL,
+	ID_country INT NOT NULL,
+	Constraint FK_country_city FOREIGN KEY (ID_country) references Country (ID),
+	Constraint PK_City PRIMARY KEY (ID));
+
+CREATE TABLE Hotel (
+	ID INT NOT NULL identity,
+	Name varchar(255) NOT NULL,
+	Class INT NOT NULL,
+	Description varchar(255) ,
+	ID_city INT NOT NULL,
+	Constraint FK_city_hotel FOREIGN KEY (ID_city) references City (ID),
+	Constraint PK_Hotel PRIMARY KEY (ID));
+
+CREATE TABLE Transfer_type (
+	ID INT NOT NULL identity,
+	Name varchar(255) NOT NULL,
+	Constraint PK_Transfer_type  PRIMARY KEY (ID));
+
+CREATE TABLE Tour (
+	ID INT NOT NULL identity,
+	Duration INT NOT NULL,
+	Resort varchar(255) NOT NULL,
+	Price INT NOT NULL,
+	Description varchar(255) ,
+	ID_city INT NOT NULL,
+	ID_hotel INT NOT NULL,
+	ID_Transfer_type INT NOT NULL,
+	Constraint FK_city_tour FOREIGN KEY (ID_city) references City (ID),
+	Constraint FK_hotel_tour FOREIGN KEY (ID_hotel) references Hotel (ID),
+	Constraint FK_Transfer_type_tour FOREIGN KEY (ID_Transfer_type) references Transfer_type (ID),
+	Constraint PK_tour  PRIMARY KEY (ID));
+
+		
+CREATE TABLE Voucher (
+	ID INT NOT NULL identity,
+	ID_person INT NOT NULL,
+	ID_tour INT NOT NULL,
+	ID_manager INT NOT NULL,
+	start_date DATETIME NOT NULL,
+	End_date DATETIME NOT NULL,
+	Number_of_people INT NOT NULL,
+	Constraint FK_person_voucher FOREIGN KEY (ID_person) references Person (ID),
+	Constraint FK_tour_voucher FOREIGN KEY (ID_tour) references Tour (ID),
+	Constraint FK_manager_voucher FOREIGN KEY (ID_manager) references Manager (ID),
+	Constraint PK_voucher PRIMARY KEY (ID));
+
+
+CREATE TABLE Membership (
+	ID INT NOT NULL identity,
+	ID_voucher INT NOT NULL,
+	ID_person INT NOT NULL,
+	Name varchar(255) NOT NULL,
+	Last_name varchar(255) NOT NULL,
+	Middle_name varchar(255) NOT NULL,
+	Date_of_birth varchar(255) NOT NULL,
+	Constraint FK_voucher_membership FOREIGN KEY (ID_voucher) references Voucher (ID),
+	Constraint FK_person_membership FOREIGN KEY (ID_person ) references Person (ID),
+	Constraint PK_membership PRIMARY KEY (ID));
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ insert database.sql
@@ -0,0 +1,64 @@
+
+
+USE Travel_agency;
+
+INSERT INTO Passport_type (Discription)
+ VALUES('Birth_Certificate'),('Passport_of_a_citizen_of_Ukraine'),('Foreign_passport');
+
+
+INSERT INTO  Person  (Middle_name, [Name], Last_name, [Address],Mobile_number ,Date_of_birth)
+VALUES
+ ('Abramovuch', 'Farlay', 'Wheadon', '937 Crowley Crossing', '640-286-8660', '1990-12-23'),
+ ('Petrivna', 'Hildy', 'Garwood', '3 Warbler Hill', '386-246-9885', '1954-08-04'),
+ ('Yurivna', 'Marya', 'Casel', '8 Mesta Pass', '977-702-9108', '1982-11-22'),
+ ('Olegovuch', 'Dennet', 'Conville', '7513 Canary Lane', '299-223-3123', '2004-12-12'),
+ ('Olegovuch', 'Gretchen', 'Elsley', '99391 Lunder Plaza', '947-832-0385', '1991-10-12'),
+ ('Yurivna', 'Josi', 'Glew', '5 Buell Terrace', '324-945-3216', '1982-10-25'),
+ ('Petrivna', 'Violante', 'Hannigan', '10944 Thackeray Hill', '675-819-8039', '1996-08-19'),
+ ('Abramovuch', 'Ivett', 'Marjanovic', '7 Grim Way', '281-590-3444', '1971-05-30'),
+ ('Igorivna', 'Alayne', 'Laughtisse', '45077 Lawn Park', '823-320-3807', '2001-03-13'),
+ ('Gnatovych', 'Jacob', 'Shaves', '987 Warrior Avenue', '357-509-0266', '2017-07-11'),
+ ('Gnatovych', 'Sandro', 'Risom', '49266 Morrow Trail', '433-692-4282', '1997-02-06'),
+ ('Yurivna', 'Robena', 'Bertin', '5 Susan Terrace', '898-302-4054', '1999-05-16'),
+ ('Yurivna', 'Antonietta', 'Greschke', '8 Schlimgen Alley', '352-730-8794', '1973-08-24'),
+ ('Yurivna', 'Rosie', 'MacRannell', '9658 Caliangt Parkway', '732-524-9829', '2011-10-09'),
+ ('Olegivna', 'Marcelia', 'MacCaffery', '262 Fieldstone Point', '785-339-2095', '2018-10-30'),
+ ('Olegivna', 'Ericka', 'Messitt', '7875 Arrowood Road', '559-590-0094', '1959-11-11'),
+ ('Gnatovych', 'Alano', 'Connop', '3321 7th Avenue', '363-616-2689', '1998-08-01'),
+ ('Gnatovych', 'Hill', 'Morsley', '58 Iowa Point', '773-315-2932', '1988-04-07'),
+ ('Gnatovych', 'Christabel', 'Fugere', '358 Sycamore Trail', '873-575-0915', '2007-02-12'),
+ ('Denusovich', 'Maryann', 'Tidbold', '46 Golf View Lane', '704-402-0518', '1981-10-13'),
+ ('Vasulivna', 'Selby', 'Rudsdell', '80 Coolidge Court', '905-762-0535', '2002-01-31'),
+ ('Vasulivna', 'Dehlia', 'Gallandre', '68 Morning Crossing', '821-951-6235', '1973-08-25'),
+ ('Vasulivna', 'Camella', 'Moyer', '923 Delaware Trail', '818-381-8921', '1969-09-04'),
+ ('Markivna', 'Rubia', 'Satcher', '60930 Nevada Drive', '674-496-4171', '1969-11-17'),
+ ('Semenovuch', 'Wilt', 'Dmytryk', '229 Darwin Trail', '550-837-3925', '1976-03-02'),
+ ('Semenovuch', 'Abey', 'Goldstein', '54 Judy Lane', '764-752-7055', '1983-10-13'),
+ ('Ernestovuch', 'Franklyn', 'Tooher', '0897 Sunnyside Road', '967-859-4552', '2011-12-20'),
+ ('Ernestovuch', 'Berky', 'Mullally', '48 Johnson Plaza', '350-383-8650', '2010-03-31'),
+ ('Ernestovuch', 'Tony', 'Mumm', '798 Bluestem Way', '468-466-9451', '1987-12-10'),
+ ('Levovuch', 'Hoebart', 'Domanek', '97 Dawn Terrace', '197-991-3088', '1991-11-28'),
+ ('Fedorivna', 'Sonny', 'MacKinnon', '4629 Vidon Avenue', '593-610-5657', '1950-01-14'),
+ ('Leonidivna', 'Sabine', 'Petken', '1 Grayhawk Crossing', '125-659-7647', '2016-07-07'),
+ ('Gyliverovuch', 'Guinevere', 'McCathy', '9 Old Gate Plaza', '265-891-4017', '1996-01-07'),
+ ('Gyliverovuch', 'Jean', 'Sambeck', '57078 David Center', '864-803-2428', '2017-05-30'),
+ ('Artemivna', 'Aprilette', 'McArd', '97676 Shopko Pass', '551-608-7951', '1995-07-06'),
+ ('Semenovuch', 'Friedrick', 'Gun', '50820 Goodland Parkway', '839-772-4395', '2004-07-17'),
+ ('Artemivna', 'Findley', 'Cremin', '622 Sutherland Road', '420-983-7062', '1994-04-24'),
+ ('Gyliverovuch', 'Rochester', 'Casburn', '71 Sage Road', '385-660-8552', '1951-05-01'),
+ ('Semenovuch', 'Bryon', 'Nel', '5 Bobwhite Point', '946-669-8948', '1983-03-02'),
+ ('Semenovuch', 'Mitchel', 'Beebe', '72 Nova Junction', '796-405-5043', '2002-10-11'),
+ ('Leonidivna', 'Rafa', 'Sauvan', '37604 Melby Drive', '670-616-8582', '1956-01-24'),
+ ('Artemivna', 'Liesa', 'Hughlock', '761 Bartillon Way', '728-504-9516', '1950-08-14'),
+ ('Karlovuch', 'Germain', 'Wykey', '356 Scoville Pass', '742-766-9881', '2000-09-29'),
+ ('Artemivna', 'Prissie', 'Berrygun', '6 Cordelia Hill', '150-296-3433', '2012-09-30'),
+ ('Orestivna', 'Erin', 'Oxer', '0783 Vermont Circle', '272-955-7589', '2017-05-25'),
+ ('Orestivna', 'Correna', 'Dracksford', '75 Esker Center', '206-678-2482', '1980-07-03'),
+ ('Leonidivna', 'Isabelita', 'Rawsthorne', '1 Havey Road', '566-382-6197', '2014-02-26'),
+ ('Karlovuch', 'Svend', 'Scholling', '2 5th Point', '152-172-5627', '1982-09-02'),
+ ('Dmutrivna', 'Ase', 'Graysmark', '5 Jay Point', '839-482-1853', '1969-01-10'),
+ ('Karlovuch', 'Fletcher', 'Girodon', '2 Redwing Drive', '197-513-1413', '1984-11-22');
+
+
+
+ drop database Travel_agency
