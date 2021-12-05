cbea1536914182490f6119fe620c41c202b589ab
MartaAndriychak <bolubash@meta.ua>
2021-12-05 17:20:57 +0200

    Create Procedure

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+USE Travel_agency;
+GO
+
+CREATE PROCEDURE Select_country @Country VARCHAR(100)
+AS
+SELECT 
+   Country,
+   COUNT(Country) AS Visit
+FROM Voucher AS V
+LEFT JOIN Tour AS T ON V.ID_tour= T.ID
+LEFT JOIN City AS S ON T.ID_city= S.ID
+LEFT JOIN Country  AS C ON S.ID_country=C.ID
+WHERE C.Country= @Country
+GROUP BY Country;
+
+GO
+
+EXEC Select_country @Country= 'Egypt';
+GO
