821db1c5f977597a3e05d05666af67f59e6e5c44
MartaAndriychak <bolubash@meta.ua>
2021-12-16 10:59:14 +0200

    gvcfcfvnvvvgttrederderw3efeswesse

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -17,3 +17,18 @@ GO
 
 EXEC Select_country @Country= 'Egypt';
 GO
+
+
+CREATE FUNCTION Select_country (@Country VARCHAR(100))
+RETURN  TABLE
+AS
+RETURN
+SELECT
+   Country,
+   COUNT(Country) AS Visit
+FROM Voucher AS V
+LEFT JOIN Tour AS T ON V.ID_tour= T.ID
+LEFT JOIN City AS S ON T.ID_city= S.ID
+LEFT JOIN Country  AS C ON S.ID_country=C.ID
+WHERE C.Country= @Country
+GROUP BY Country;
